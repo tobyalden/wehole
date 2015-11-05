3df4c2cc6b587ec4879a200c74ddf5c779acb66e
Toby Alden <toby.alden@gmail.com>
2015-11-05 10:15:18 -0800

    Server recieves user events.

---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// io.on('connection', function(req, res) {
+// 	console.log('a user connected');
+// });
+
+// io.on('connection', function(socket) {
+// 	console.log('a user connected');
+// 	socket.on('disconnect', function() {
+// 		console.log('a user disconnected');
+// 	});
+// });
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('a user disconnected');
+	});
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function() {
+	console.log('listening on *:3000');
+});
