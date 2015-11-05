2fc87af67272fc707f5eca048ce2ab1bc716b6e3
Toby Alden <toby.alden@gmail.com>
2015-11-05 10:19:55 -0800

    Working chat app. No names.

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ io.on('connection', function(socket){
 		console.log('a user disconnected');
 	});
   socket.on('chat message', function(msg){
+  	io.emit('chat message', msg);
     console.log('message: ' + msg);
   });
 });
