e7e7f0c390e230b82613a08892a2b7fdd1db52c6
Toby Alden <toby.alden@gmail.com>
2015-11-05 14:41:03 -0800

    Server replays video over and over.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var YouTube = require('youtube-node');
+var youTube = new YouTube();
+youTube.setKey('AIzaSyAeOVDwLCaSSKjDWf1gOGPJhKYal3ZHIP8');
+youTube.addParam('videoDuration');
+var moment = require('moment');
+moment().format();
+
+// var $ = require('jQuery');
+// var najax = require('najax');
+// var chance = require('chance').Chance();
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -10,10 +20,24 @@ io.on('connection', function(socket){
 	console.log('A user connected.');
 
   socket.on('video request', function() {
-    console.log('Video request recieved. Sending video...');
-    var video = 'U_kqX_gFiQE';
-    io.emit('video', video);
+    console.log('Video request recieved.');
+    restartVideo();
   });
+
+  function restartVideo() {
+    var video = 'rZtGLNQWmCc';
+    youTube.getById(video, function(error, result) {
+      if (error) {
+        console.log(error);
+      }
+      else {
+        var duration = moment.duration(result.items[0].contentDetails.duration).asMilliseconds();
+        console.log("Video length: " + duration + "ms");
+        setTimeout(restartVideo, duration);
+      }
+    });
+    io.emit('video', video);
+  }
 
   socket.on('chat message', function(msg){
   	io.emit('chat message', msg);
