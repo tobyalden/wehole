d83ef75dd21f930e4d2e6de2495c1c14d5180bb9
Toby Alden <toby.alden@gmail.com>
2015-11-05 12:04:59 -0800

    Client requests video, server gives video, client plays video.

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var repeat = require('repeat');
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function() {
-		console.log('a user disconnected');
-	});
+	console.log('A user connected.');
+
+  socket.on('video request', function() {
+    console.log('Video request recieved. Sending video...');
+    var video = 'U_kqX_gFiQE';
+    io.emit('video', video);
+  });
+
   socket.on('chat message', function(msg){
   	io.emit('chat message', msg);
-    console.log('message: ' + msg);
+    console.log('Message: ' + msg);
   });
+
+  socket.on('disconnect', function() {
+    console.log('A user disconnected.');
+  });
+
 });
 
 http.listen(3000, function() {
-	console.log('listening on *:3000');
+	console.log('Listening on *:3000.');
 });
-
-// function sayHello() {
-//   console.log("Hello world!");
-// };
- 
-// repeat(sayHello).every(500, 'ms').for(2, 'minutes').start.in(5, 'sec');
